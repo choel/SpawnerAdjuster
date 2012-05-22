a976a867bbd4ec215eaa68b97d49ee53dc0aa8c6
Tyler McNair <sadmean@sadmean.com>
2012-05-22 11:52:40 -0600

    RC build

--- src/main/java/com/sadmean/mc/SpawnerAdjuster/AdjusterBlockListener.java
+++ src/main/java/com/sadmean/mc/SpawnerAdjuster/AdjusterBlockListener.java
@@ -126,7 +126,7 @@ public class AdjusterBlockListener implements Listener {
 				}
 			}
 			if(SpawnerAdjuster.permCheck(event.getPlayer(), "SpawnerAdjuster.DropSpawners") && SpawnerAdjuster.allowDroppedSpawners) {
-				if(!event.isCancelled()) {
+				if(event.isCancelled() == false) {
 					short asdf = 1;
 					ItemStack spawnerstack = new ItemStack(event.getBlock().getType(), 1, asdf, event.getBlock().getData());
 					event.getBlock().getLocation().getWorld().dropItemNaturally(event.getBlock().getLocation(), spawnerstack);
